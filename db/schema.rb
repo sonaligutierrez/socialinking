ccfdfdced39125cef757159d4ff8321ab0e6764f
Betsabeth Torres <kyaalena@railsgirls.gnu.ve>
2018-05-15 23:36:28 -0400

    database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506134000) do
+ActiveRecord::Schema.define(version: 20180515190548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "commentaries", force: :cascade do |t|
+    t.date "date"
+    t.text "commentary"
+    t.bigint "facebook_users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "posts_id"
+    t.index ["facebook_users_id"], name: "index_commentaries_on_facebook_users_id"
+    t.index ["posts_id"], name: "index_commentaries_on_posts_id"
+  end
+
+  create_table "facebook_users", force: :cascade do |t|
+    t.string "fb_username"
+    t.string "fb_name"
+    t.string "fb_avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_creators", force: :cascade do |t|
+    t.string "fan_page"
+    t.string "url"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.date "date"
+    t.date "post_date"
+    t.bigint "post_creators_id"
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_creators_id"], name: "index_posts_on_post_creators_id"
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
+
+  create_table "scraping_logs", force: :cascade do |t|
+    t.date "scraping_date"
+    t.time "exec_time"
+    t.integer "total_comment"
+    t.bigint "posts_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["posts_id"], name: "index_scraping_logs_on_posts_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +80,9 @@ ActiveRecord::Schema.define(version: 20180506134000) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "commentaries", "facebook_users", column: "facebook_users_id"
+  add_foreign_key "commentaries", "posts", column: "posts_id"
+  add_foreign_key "posts", "post_creators", column: "post_creators_id"
+  add_foreign_key "posts", "users", column: "users_id"
+  add_foreign_key "scraping_logs", "posts", column: "posts_id"
 end
