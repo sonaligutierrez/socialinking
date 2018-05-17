0a7091dc26974d1a5148219d47b5f58ee05ec27a
Betsabeth Torres <kyaalena@railsgirls.gnu.ve>
2018-05-17 08:19:19 -0400

    change single quotes with double quotes in call me test_helper method

--- test/models/facebook_user_test.rb
+++ test/models/facebook_user_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class FacebookUserTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/models/scraping_log_test.rb
+++ test/models/scraping_log_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class ScrapingLogTest < ActiveSupport::TestCase
   # test "the truth" do
