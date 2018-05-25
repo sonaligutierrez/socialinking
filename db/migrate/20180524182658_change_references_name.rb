4582a5ab633b9f1ed2c13af0dfb6b885c367aeea
sonaligutierrez <sogu@gmail.com>
2018-05-25 09:11:15 -0400

    fix error with rubocop

--- db/migrate/20180524182658_change_references_name.rb
+++ db/migrate/20180524182658_change_references_name.rb
@@ -1,29 +1,26 @@
 class ChangeReferencesName < ActiveRecord::Migration[5.1]
   def self.up
-  	remove_foreign_key :posts, column: :accounts_id
-  	remove_column :posts, :accounts_id
-  	add_reference :posts, :account, foreign_key: true
+    remove_foreign_key :posts, column: :accounts_id
+    remove_column :posts, :accounts_id
+    add_reference :posts, :account, foreign_key: true
 
-  	remove_foreign_key :posts, column: :post_creators_id
-  	remove_column :posts, :post_creators_id
-  	add_reference :posts, :post_creator, foreign_key: true
+    remove_foreign_key :posts, column: :post_creators_id
+    remove_column :posts, :post_creators_id
+    add_reference :posts, :post_creator, foreign_key: true
 
-  	remove_foreign_key :scraping_logs, column: :posts_id
-  	remove_column :scraping_logs, :posts_id
-  	add_reference :scraping_logs, :post, foreign_key: true
-
-
+    remove_foreign_key :scraping_logs, column: :posts_id
+    remove_column :scraping_logs, :posts_id
+    add_reference :scraping_logs, :post, foreign_key: true
   end
 
   def self.down
-  	remove_foreign_key :posts, :account, foreign_key: true
-  	add_reference :posts, column: :accounts_id, foreign_key: true
+    remove_foreign_key :posts, :account, foreign_key: true
+    add_reference :posts, column: :accounts_id, foreign_key: true
 
-  	remove_foreign_key :posts, :post_creator, foreign_key: true
-  	add_reference :posts, column: :post_creators, foreign_key: true
+    remove_foreign_key :posts, :post_creator, foreign_key: true
+    add_reference :posts, column: :post_creators, foreign_key: true
 
-	remove_foreign_key :scraping_logs, :post, foreign_key: true
-  	add_reference :scraping_logs, column: :posts, foreign_key: true
-
+    remove_foreign_key :scraping_logs, :post, foreign_key: true
+    add_reference :scraping_logs, column: :posts, foreign_key: true
   end
 end
--- db/migrate/20180524191818_add_url_column_in_posts.rb
+++ db/migrate/20180524191818_add_url_column_in_posts.rb
@@ -1,5 +1,5 @@
 class AddUrlColumnInPosts < ActiveRecord::Migration[5.1]
   def change
-  	add_column :posts, :url, :string
+    add_column :posts, :url, :string
   end
 end
