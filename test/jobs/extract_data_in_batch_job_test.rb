5205492d39aee5638cd30336fe09083c6d7837a1
Luis Hernandez <luelher@gmail.com>
2018-06-17 11:08:42 -0400

    Fix some rubocop offenses

--- test/jobs/extract_data_in_batch_job_test.rb
+++ test/jobs/extract_data_in_batch_job_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class ExtractDataInBatchJobTest < ActiveJob::TestCase
-  def setup 
+  def setup
     @fb_post = posts(:three)
   end
 
   test "scraping execution for a post" do
     VCR.use_cassette("fb_scraping") do
-      count = ExtractDataInBatchJob.perform_now @fb_post 
+      count = ExtractDataInBatchJob.perform_now @fb_post
       assert_equal(@fb_post.post_comments.count, count)
     end
   end
