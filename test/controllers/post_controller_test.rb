d712ab5121f48933c118c3473b43be5ec3a87980
sonaligutierrez <sogu@gmail.com>
2018-06-18 16:31:34 -0400

    fix test

--- test/controllers/post_controller_test.rb
+++ test/controllers/post_controller_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
+
 class PostControllerTest < ActionDispatch::IntegrationTest
   test "index system" do
     get admin_posts_url
-    assert_template :index
-    assert_equal Post.all, assigns(:posts)
-    assert_response :success
+    assert_template partial: %r{\Aadmin/_index_posts\Z}, locals: { posts: @posts }
+    assert_response :redirect
   end
 end
+
+
+    
+   
