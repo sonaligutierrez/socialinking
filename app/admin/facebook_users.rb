ad637eb3dfecd24253905a2adbb44ebfb94ec52b
sonaligutierrez <sogu@gmail.com>
2018-07-04 13:37:45 -0400

    fix issue-2

--- app/admin/facebook_users.rb
+++ app/admin/facebook_users.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register FacebookUser do
   menu label: proc { I18n.t("active_admin.facebook_users") }, priority: 4
   permit_params :fb_username, :fb_name, :fb_avatar
-
+  filter :fb_username
+  filter :fb_name
+  filter :fb_avatar
   index do
       column :fb_username
       column :fb_name
@@ -10,3 +12,4 @@ ActiveAdmin.register FacebookUser do
     end
 
 end
+
