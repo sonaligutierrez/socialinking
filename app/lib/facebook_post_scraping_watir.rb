6a87f186e47ae476f33562332f895ef01c79b3db
Luis Hernandez <luelher@gmail.com>
2018-10-04 17:25:04 -0300

    Increment scraping time out

--- app/lib/facebook_post_scraping_watir.rb
+++ app/lib/facebook_post_scraping_watir.rb
@@ -10,7 +10,7 @@
 class FacebookPostScrapingWatir
   attr_accessor :post_url, :browser, :fb_user, :fb_pass, :comments, :page, :finish_paging, :cookie_json, :proxy, :debug, :start_time, :headless, :message
 
-  MAX_SCRAPING_TIME = 400 # sec
+  MAX_SCRAPING_TIME = 1200 # sec
 
   def initialize(post_url, user, pass, cookie_json, proxy, headless = true, debug = false)
     @post_url = post_url
@@ -212,7 +212,8 @@ class FacebookPostScrapingWatir
         sleep 3
         if get_execution_time > MAX_SCRAPING_TIME
           @message += "Scraping Time up. "
-          return result_comments
+          @start_time = nil
+          break
         end
       end
 
